5c1d506b282853423b395a2c918a5700d1cc427f
Gijs Pampiermole <69118714+GijsPampiermole@users.noreply.github.com>
2024-09-13 13:45:14 +0200

    fixed type + ensure installed config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,9 +3,18 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-      local config = require("nvim-treesitter.config")
+      local config = require("nvim-treesitter.configs")
       config.setup({
         auto_install = true,
+        ensure_installed = {
+          "lua", 
+          "bash", 
+          "dockerfile",
+          "python",
+          -- experimental
+          "markdown_inline",
+          "yaml"
+        },
         highlight = { enable = true },
         indent = { enable = true },
       })
